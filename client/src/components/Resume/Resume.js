4afba74fcc315e4ee52bd468410801ccd9d862e9
Chris Reed <cr31293@gmail.com>
2020-11-02 21:03:52 -0500

    resume added

---
+++ client/src/components/Resume/Resume.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import CardMedia from '@material-ui/core/CardMedia';
+
+const useStyles = makeStyles({
+  media: {
+    width: "auto",
+    height: "auto",
+    margin: "auto",
+    marginBottom: "5%",
+    marginTop: "1.5%"
+  },
+});
+
+
+function ResumeDoc() {
+  const classes = useStyles();
+
+  return (
+    <>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          image="/images/resume.png"
+          title="Contemplative Reptile"
+          className={classes.media}
+        />
+    </>
+  );
+}
+
+export default ResumeDoc;
--- client/src/pages/Resume/index.js
+++ client/src/pages/Resume/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Hero from '../../components/Hero';
 import Header from "../../components/Header";
-
+import ResumeDoc from "../../components/Resume/Resume";
 
 function Resume () {
 
     return (
         <React.Fragment>
             <Header />
-            <Hero />
+            <ResumeDoc />
             
         </React.Fragment>
     );
