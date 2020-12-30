52ccb871d8b096f115ed80714b03b60e5d679c48
Chris Reed <cr31293@gmail.com>
2020-12-30 15:37:36 -0500

    adding theme breakpoints for portfolio page styling

--- client/src/components/PortfolioGrid/PortfolioGrid.js
+++ client/src/components/PortfolioGrid/PortfolioGrid.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
@@ -9,6 +9,7 @@ import InfoIcon from "@material-ui/icons/Info";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import tileData from "./tileData.json";
 import Link from "@material-ui/core/Link";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,11 +17,13 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: "wrap",
     justifyContent: "space-around",
     overflow: "hidden",
-    boxSizing: "content-box",
   },
   gridList: {
     height: "80%",
-    width: "60%",
+    [theme.breakpoints.down("md")]: {
+      width: "100%"
+    },
+    width: "50%",
     overflowY: "auto",
     margin: 0,
     padding: 0,
@@ -44,26 +47,43 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PortfolioGrid() {
   const classes = useStyles();
+  const theme = useTheme();
+  const colMatches = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const mobileCol = 1;
+  const nonMobileCol = 2;
+  const gridListTileDisplay = colMatches ? "listItems" : "flex";
+  const gridListTileWidth = colMatches ? "70%" : "50%";
 
   return (
     <div className={classes.root}>
       <GridList cellHeight={"auto"} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} >
+        <GridListTile 
+          key="Subheader" 
+          cols={colMatches ? 1 : 2 } 
+          
+        >
           <ListSubheader component="div"></ListSubheader>
         </GridListTile>
         {tileData.map((tile) => (
           <GridListTile
             key={tile.image}
             style={{
+              width: gridListTileWidth,
               height: "15rem",
               paddingRight: "1rem",
               paddingLeft: "1rem",
               paddingBottom: "2rem",
+              display: gridListTileDisplay,
+              margin: ""
             }}
           >
             <img src={process.env.PUBLIC_URL + tile.image} alt={tile.title} />
             <GridListTileBar
               className={classes.tileBar}
+              style={{
+                backgroundColor: "rgba(0, 0, 0, .85)"
+              }}
               title={tile.title}
               subtitle={<span>by: {tile.author}</span>}
               actionIcon={
