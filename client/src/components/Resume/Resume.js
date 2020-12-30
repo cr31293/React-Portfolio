6482d551b4333c779c31e567b028f445cd07fdff
Chris Reed <cr31293@gmail.com>
2020-12-30 16:04:48 -0500

    changes to resume img width for responsive design

--- client/src/components/Resume/Resume.js
+++ client/src/components/Resume/Resume.js
@@ -2,21 +2,22 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import CardMedia from '@material-ui/core/CardMedia';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   media: {
-    width: "-webkit-fill-available",
-    height: "fit-conent",
+    [theme.breakpoints.down("md")]: {
+      width: "-webkit-fill-available",
+    },
+    width: "auto",
     margin: "auto",
     marginBottom: "5%",
     marginTop: "7em"
 
   },
-});
+}));
 
 
 function ResumeDoc() {
   const classes = useStyles();
-
   return (
     <>
         <CardMedia
